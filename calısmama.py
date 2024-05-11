26aba5d5ad4d26a3f5c22743535d2324b55253da
A7IHAN <alihan.ozturk@icloud.com>
2024-05-11 23:30:29 +0300

    a

---
+++ calısmama.py
@@ -0,0 +1,172 @@
+###########################################################
+# IMPORT'LAR VE BAZI AYARLAR #
+###########################################################
+
+import warnings
+import pandas as pd
+from datetime import datetime
+from prophet import Prophet
+from prophet.diagnostics import cross_validation
+from prophet.diagnostics import performance_metrics
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+import numpy as np
+from hyperopt import hp, fmin, tpe, Trials
+import veri_seti as vs
+
+warnings.filterwarnings("ignore")
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', 500)
+
+
+###########################################################
+# MODELLEME #
+###########################################################
+from sklearn.preprocessing import StandardScaler
+
+
+
+def dataf_for_prophet(dataframe, y):
+    dataframe["ds"] = dataframe.index
+    dataframe["y"] = dataframe[y]
+    dataframe.drop([y], axis=1, inplace=True)
+    dataframe.columns = dataframe.columns.map('_'.join)
+    dataframe.rename(columns={"y_": "y", "ds_": "ds"}, inplace=True)
+    return dataframe
+
+
+df = dataf_for_prophet(vs.df_clean, ("KCHOL.IS", "Close"))
+
+df.drop("ds", axis=1, inplace=True)
+
+
+scaler = StandardScaler()
+scaled_data = scaler.fit_transform(df)
+
+dfss = pd.DataFrame(scaled_data, columns=df.columns)
+dfss["ds"] = df.index
+df["ds"] = df.index
+
+
+# EN İYİ ÇAPRAZ DOĞRULAMA DEĞERLERİNİ BULMA #
+
+def objective0(params):
+    initial = params['initial']
+    period = params['period']
+    horizon = params['horizon']
+
+    model0 = Prophet()
+    model0.fit(dfss)
+    df_cv0 = cross_validation(model0, initial=initial, period=period, horizon=horizon)
+    df_p0 = performance_metrics(df_cv0)
+    return -df_p0['rmse'].values[0]  # Çapraz doğrulama performansını maksimize et
+
+
+hyper_iph = {
+    'initial': hp.choice('initial', ["15 days", "30 days", "45 days", '90 days', '180 days',
+                                     '365 days', '730 days', '1095 days']),
+    'period': hp.choice('period', ['45 days', '90 days', '180 days',
+                                   '365 days', '730 days', '1095 days']),
+    'horizon': hp.choice('horizon', ['45 days', '90 days',
+                                     '180 days', '365 days', '730 days', '1095 days'])
+}
+
+trials0 = Trials()
+last_iph = fmin(objective0, hyper_iph, algo=tpe.suggest, max_evals=10, trials=trials0)
+
+best_iph = {
+    'initial': ["15 days", "30 days", '45 days', '90 days', '180 days', '365 days', '730 days', '1095 days',][last_iph['initial']],
+    'period': ['45 days', '90 days', '180 days', '365 days', '730 days', '1095 days'][last_iph['period']],
+    'horizon': ['45 days', '90 days', '180 days', '365 days', '730 days', '1095 days'][last_iph['horizon']]
+}
+
+print(best_iph)
+
+
+# HİPERPARAMETRE OPTİMİZASYONU #
+
+def objective1(params):
+    model1 = Prophet(**params)
+    model1.fit(dfss)  # Modeli eğitelim
+    df_cv1 = cross_validation(model1, initial=best_iph["initial"], period=best_iph["period"],
+                              horizon=best_iph["horizon"])
+    df_p1 = performance_metrics(df_cv1)
+    return -df_p1['rmse'].values[0]
+
+
+hyperparam_space = {
+    'changepoint_prior_scale': hp.uniform('changepoint_prior_scale', 0.001, 0.5),
+    'seasonality_prior_scale': hp.uniform('seasonality_prior_scale', 0.01, 10.0),
+    'holidays_prior_scale': hp.uniform('holidays_prior_scale', 0.01, 10.0),
+    'seasonality_mode': hp.choice('seasonality_mode', ['additive', 'multiplicative'])}
+
+
+trials1 = Trials()
+best = fmin(objective1, hyperparam_space, algo=tpe.suggest, max_evals=10, trials=trials1)
+best['seasonality_mode'] = "additive" if best['seasonality_mode'] == 0 else "multiplicative"
+
+print(best)
+
+
+
+###########################################################
+# EN İYİ MODEL İLE TAHMİNLEME #
+###########################################################
+
+model_best = Prophet(**best)
+model_best.fit(dfss)
+
+future = model_best.make_future_dataframe(periods=730)
+predicts = model_best.predict(future)
+predicts0 = predicts.drop(columns="ds")
+predicts01 = scaler.inverse_transform(predicts0["yhat"])
+
+
+# SKORLAMA #
+
+y_true = df["y"]
+y_pred = predicts["yhat"][:-730]
+
+r2 = r2_score(y_true, y_pred)
+
+rmse = np.sqrt(mean_squared_error(y_true, y_pred))
+
+print("R^2 (R-kare):", r2)
+print("RMSE:", rmse)
+
+
+###########################################################
+# ÖZELLİK ÖNEMİ #
+###########################################################
+
+model_best.plot_components(predicts)
+plt.show()
+
+###########################################################
+# GÖRSELLEŞTİRME #
+###########################################################
+
+# Tahminlenen kısmı gerçek değerlerden farklı olarak görselleştir
+fig = go.Figure()
+
+# Gerçek verileri ekle
+fig.add_trace(go.Scatter(x=df.index, y=df['y'], mode='lines', name='Gerçek Veriler'))
+
+# Tahminleri ekle
+fig.add_trace(go.Scatter(x=predicts['ds'], y=predicts['yhat'], mode='lines', name='Tahminlenen Veriler'))
+
+# İleriye dönük tahminleri vurgula
+fig.add_trace(go.Scatter(x=predicts['ds'].iloc[-730:], y=predicts['yhat'].iloc[-730:], mode='lines', fill='tozeroy', fillcolor='rgba(0, 0, 255, 0.3)', name='İleriye Dönük Tahminler'))
+
+# Grafik düzenleme
+fig.update_layout(title='Prophet Tahminleri', xaxis_title='Tarih', yaxis_title='Değer')
+
+# Grafiği HTML dosyası olarak kaydet
+fig.write_html("prophet_tahminleri_ss.html")
+
+
+
+
---
+++ veri_seti.py
@@ -0,0 +1,115 @@
+###########################################################
+# IMPORT'LAR VE BAZI AYARLAR #
+###########################################################
+
+import yfinance as yf
+import warnings
+import pandas as pd
+import evds as e
+from datetime import datetime, timedelta
+
+warnings.filterwarnings("ignore")
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', 500)
+
+###########################################################
+# VERİ SETİ OLUŞTURMA #
+###########################################################
+
+# EVDS API ANAHTARI
+key = "R0tm8EWAmS"
+evds = e.evdsAPI(key)
+
+
+# KUR SEPETİ VERİSİ #
+
+exc = evds.get_data(["TP.DK.USD.S.YTL", "TP.DK.EUR.S.YTL"],
+                    startdate="01-01-2018",
+                    enddate="31-12-2023",
+                    frequency=1, aggregation_types="last")
+
+exc["KUR_SEPETİ"] = (exc["TP_DK_USD_S_YTL"] / 2) + (exc["TP_DK_EUR_S_YTL"] / 2)
+
+
+# ENFLASYON VERİSİ #
+
+enf = evds.get_data(["TP.FG.J0", "TP.TUFE1YI.T1"],
+                    startdate="01-01-2018",
+                    enddate="31-12-2023",
+                    frequency=1)
+
+enf.rename(columns={"TP_FG_J0": "TUFE", "TP_TUFE1YI_T1": "UFE"}, inplace=True)
+
+daily_enf = pd.DataFrame(columns=["Tarih", "TUFE"])
+
+for index, row in enf.iterrows():
+    year, month = map(int, row["Tarih"].split("-"))
+    days_in_month = (datetime(year, month % 12 + 1, 1) - timedelta(days=1)).day
+    daily_increase_tufe = (row["TUFE"] - enf.iloc[index - 1]["TUFE"]) / days_in_month
+    daily_data = []
+    for day in range(1, days_in_month + 1):
+        date = f"{year}-{month:02d}-{day:02d}"
+        daily_tufe = row["TUFE"] - daily_increase_tufe * (days_in_month - day)
+        daily_data.append({"Tarih": date, "TUFE": daily_tufe})
+    daily_enf = pd.concat([daily_enf, pd.DataFrame(daily_data)])
+
+
+"""
+AŞAĞIDAKİ BLOK REPO YENİDEN TARİHLEME İŞLEMİ İÇİN KULLANILIYOR.
+"""
+daily_enf["Tarih"] = pd.to_datetime(daily_enf["Tarih"], format="%Y-%m-%d")
+
+min_tarih = daily_enf["Tarih"].min()
+max_tarih = daily_enf["Tarih"].max()
+
+
+# REPO VERİSİ #
+
+rep0 = evds.get_data(["TP.AOFOBAP"],
+                     startdate="01-01-2018",
+                     enddate="31-12-2023",
+                     frequency=1)
+
+rep0["Tarih"] = pd.to_datetime(rep0["Tarih"], format='%d-%m-%Y')
+rep0.set_index("Tarih", inplace=True)
+
+tum_tarihler = pd.date_range(start=min_tarih, end=max_tarih)
+tam_df = pd.DataFrame({"Tarih": tum_tarihler})
+repo = pd.merge(tam_df, rep0, on="Tarih", how='left')
+
+repo.rename(columns={"TP_AOFOBAP": "REPO"}, inplace=True)
+
+
+# YABANCI, YERLİ TAKAS VERİSİ #
+
+"""
+AYLIĞA ÇEVRİLİP VERİ SETİNE EKLENECEK
+"""
+
+takas = pd.read_excel("ALL_INVESTOR.xlsx", index_col="TARIH")
+
+
+# HİSSE, BİST100 VB. BORSA VERİLERİ #
+
+stocks = ["KCHOL.IS", "XU100.IS", "BZ=F", "GC=F", "BTC-USD"]
+
+data = yf.download(stocks, start="2018-01-01", end="2024-01-01", group_by="ticker")
+
+
+###########################################################
+# VERİ SETİNİN BİRLEŞTİRİLMESİ VE DÜZENLENMESİ #
+###########################################################
+
+data[("OTHER_VALUES", "EXCHANGE_BASKET")] = exc["KUR_SEPETİ"].values
+data[("OTHER_VALUES", "TUFE")] = daily_enf["TUFE"].values
+data[("OTHER_VALUES", "REPO")] = repo["REPO"].values
+
+df_clean = data.dropna(subset=[("KCHOL.IS", "Close")])
+
+df_clean.fillna(method="ffill", inplace=True)
+df_clean.fillna(method="bfill", inplace=True)
+
+for i in range(1, 8):
+    yeni_sutun = f"LAG_{i}"
+    df_clean[("LAGS", yeni_sutun)] = df_clean[("KCHOL.IS", "Close")].shift(i)
